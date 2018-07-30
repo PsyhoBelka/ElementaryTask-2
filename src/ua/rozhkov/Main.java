06b1e97e789b1b1c0e1131126f3ee3caf7f13b42
PsyhoBelka <agroghkov@gmail.com>
2018-07-30 11:11:11 +0300

    Add method compareEnv to Envelop

--- src/ua/rozhkov/Main.java
+++ src/ua/rozhkov/Main.java
@@ -9,32 +9,30 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         do {
-            Convert convert1 = new Convert();
+            Envelop envelop1 = new Envelop();
             System.out.print("Input width of Convert1: ");
-            convert1.setWidth(scanner.nextDouble());
+            envelop1.setWidth(scanner.nextDouble());
             System.out.print("Input height of Convert1: ");
-            convert1.setHeight(scanner.nextDouble());
-            System.out.println("Square of Convert 1 is " + convert1.square());
+            envelop1.setHeight(scanner.nextDouble());
+            System.out.println("Square of Envelop 1 is " + envelop1.square());
 
-            Convert convert2 = new Convert();
+            Envelop envelop2 = new Envelop();
             System.out.print("Input width of Convert2: ");
-            convert2.setWidth(scanner.nextDouble());
+            envelop2.setWidth(scanner.nextDouble());
             System.out.print("Input height of Convert2: ");
-            convert2.setHeight(scanner.nextDouble());
-            System.out.println("Square of Convert 2 is " + convert2.square());
+            envelop2.setHeight(scanner.nextDouble());
+            System.out.println("Square of Envelop 2 is " + envelop2.square());
 
-           /* if (((convert2.getWidth() < convert1.getWidth()) & (convert2.getHeight() < convert1.getHeight())) ||
-                    ((convert2.getHeight() < convert1.getWidth()) & (convert2.getWidth() < convert1.getHeight()))) {
-                System.out.println("Convert 1 can be place in Convert 2");
+            if (envelop1.compareEnv(envelop2)) {
+                System.out.println("Envelop 2 can be place in Envelop 1");
             } else {
-                System.out.println("Convert 1 can't be place in Convert 2");
-            }*/
-
-            if (convert1.square() > convert2.square()) {
-                System.out.println("Convert 1 can be place in Convert 2");
-            } else {
-                System.out.println("Convert 1 can't be place in Convert 2");
+                if (envelop2.compareEnv(envelop1)) {
+                    System.out.println("Envelop 1 can be place in Envelop 2 ");
+                } else {
+                    System.out.println("No one envelop can be place in each other");
+                }
             }
+
             System.out.println("Do you want to compare another convert pair? Type anything to repeat or exit to end: ");
         } while (!Objects.equals(scanner.next().toLowerCase(), "exit"));
     }
